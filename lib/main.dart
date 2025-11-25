dbcf3709234aabf903261f35d5d92fb1180636c5
up2294283 <bahaeldeinadam@gmail.com>
2025-11-25 16:12:56 +0000

    improve the homepage grid layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/pages/product_page.dart';
 import 'package:union_shop/pages/about_us_page.dart';
+import 'package:union_shop/pages/collections_page.dart';
+import 'package:union_shop/pages/collection_detail_page.dart';
+import 'package:union_shop/pages/sale_page.dart';
 import 'package:union_shop/widgets/custom_app_bar.dart';
 import 'package:union_shop/widgets/custom_footer.dart';
+import 'package:union_shop/widgets/product_card.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -26,6 +30,9 @@ class UnionShopApp extends StatelessWidget {
         '/': (context) => const HomeScreen(),
         '/product': (context) => const ProductPage(),
         '/about': (context) => const AboutUsPage(),
+        '/collections': (context) => const CollectionsPage(),
+        '/collection-detail': (context) => const CollectionDetailPage(),
+        '/sale': (context) => const SalePage(),
       },
     );
   }
@@ -143,6 +150,7 @@ class HomeScreen extends StatelessWidget {
                       physics: const NeverScrollableScrollPhysics(),
                       crossAxisCount:
                           MediaQuery.of(context).size.width > 600 ? 2 : 1,
+                          MediaQuery.of(context).size.width > 600 ? 4 : 2,
                       crossAxisSpacing: 24,
                       mainAxisSpacing: 48,
                       children: const [
@@ -180,63 +188,6 @@ class HomeScreen extends StatelessWidget {
             const CustomFooter(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ProductCard extends StatelessWidget {
-  final String title;
-  final String price;
-  final String imageUrl;
-
-  const ProductCard({
-    super.key,
-    required this.title,
-    required this.price,
-    required this.imageUrl,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/product');
-      },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return Container(
-                  color: Colors.grey[300],
-                  child: const Center(
-                    child: Icon(Icons.image_not_supported, color: Colors.grey),
-                  ),
-                );
-              },
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 4),
-              Text(
-                title,
-                style: const TextStyle(fontSize: 14, color: Colors.black),
-                maxLines: 2,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                price,
-                style: const TextStyle(fontSize: 13, color: Colors.grey),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
