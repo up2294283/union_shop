67877a6b1ffc3a949b80766018dfa957e16b1156
up2294283 <bahaeldeinadam@gmail.com>
2025-11-30 22:11:12 +0000

    search bottom update

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
-      ),
-      body: const Center(
-        child: Text('Search Page'),
-      ),
-    );
-  }
-}
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -3,10 +3,19 @@ import 'package:union_shop/pages/cart_page.dart';
 import 'package:union_shop/pages/collections_page.dart';
 import 'package:union_shop/pages/sale_page.dart';
 import 'package:union_shop/pages/profile_page.dart';
-import 'package:union_shop/pages/search_page.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(140);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  bool _isSearching = false;
 
   void navigateToHome(BuildContext context) {
     // Use pushNamedAndRemoveUntil to clear the navigation stack
@@ -37,77 +46,106 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
               padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Row(
                 children: [
-                  GestureDetector(
-                    onTap: () => navigateToHome(context),
-                    child: Image.network(
-                      'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
-                      height: 18,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey[300],
-                          width: 18,
-                          height: 18,
-                          child: const Center(
-                            child: Icon(Icons.image_not_supported,
-                                color: Colors.grey),
-                          ),
-                        );
+                  if (_isSearching)
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back, size: 18, color: Colors.grey),
+                      onPressed: () {
+                        setState(() {
+                          _isSearching = false;
+                        });
                       },
+                    )
+                  else
+                    GestureDetector(
+                      onTap: () => navigateToHome(context),
+                      child: Image.network(
+                        'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
+                        height: 18,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            color: Colors.grey[300],
+                            width: 18,
+                            height: 18,
+                            child: const Center(
+                              child: Icon(Icons.image_not_supported,
+                                  color: Colors.grey),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  const Spacer(),
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 600),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.search,
-                              size: 18, color: Colors.grey),
-                          padding: const EdgeInsets.all(8),
-                          constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (_) => const SearchPage()),
-                            );
-                          },
+                  if (_isSearching)
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search...',
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.clear, size: 18, color: Colors.grey),
+                              onPressed: () {
+                                // Clear search text
+                              },
+                            ),
+                          ),
                         ),
-                        IconButton(
-                          icon: const Icon(Icons.person_outline,
-                              size: 18, color: Colors.grey),
-                          padding: const EdgeInsets.all(8),
-                          constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (_) => const ProfilePage()),
-                            );
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.shopping_bag_outlined,
-                              size: 18, color: Colors.grey),
-                          padding: const EdgeInsets.all(8),
-                          constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (_) => const CartPage()),
-                            );
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.menu,
-                              size: 18, color: Colors.grey),
-                          padding: const EdgeInsets.all(8),
-                          constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
-                          onPressed: () {},
-                        ),
-                      ],
+                      ),
+                    )
+                  else
+                    const Spacer(),
+                  if (!_isSearching)
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 600),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.search,
+                                size: 18, color: Colors.grey),
+                            padding: const EdgeInsets.all(8),
+                            constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
+                            onPressed: () {
+                              setState(() {
+                                _isSearching = true;
+                              });
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.person_outline,
+                                size: 18, color: Colors.grey),
+                            padding: const EdgeInsets.all(8),
+                            constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (_) => const ProfilePage()),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.shopping_bag_outlined,
+                                size: 18, color: Colors.grey),
+                            padding: const EdgeInsets.all(8),
+                            constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (_) => const CartPage()),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.menu,
+                                size: 18, color: Colors.grey),
+                            padding: const EdgeInsets.all(8),
+                            constraints: const BoxConstraints(minWidth: 32, minHeight: 32),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
                 ],
               ),
             ),
@@ -131,9 +169,6 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       ),
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(140); // Update preferred size
 }
 
 class _NavBarItem extends StatelessWidget {
