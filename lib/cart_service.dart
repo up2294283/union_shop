012798b4bbf99aa1851d67b545c983eda96e0cae
up2294283 <bahaeldeinadam@gmail.com>
2025-11-30 19:46:42 +0000

    cart service

---
+++ lib/cart_service.dart
@@ -0,0 +1,17 @@
+import '../models/product.dart';
+
+class CartService {
+  static final List<Map<String, dynamic>> _cart = [];
+
+  static void add(Product p, int quantity) {
+    _cart.add({
+      "product": p,
+      "quantity": quantity,
+    });
+  }
+
+  static List<Map<String, dynamic>> get items => _cart;
+
+  static double get total =>
+      _cart.fold(0, (sum, item) => sum + item["product"].price * item["quantity"]);
+}
