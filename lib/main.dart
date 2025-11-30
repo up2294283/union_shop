235fa8d7c29e46738f9bb8ad74c6eff33d5a8029
up2294283 <bahaeldeinadam@gmail.com>
2025-11-30 19:40:48 +0000

    initial update

--- lib/main.dart
+++ lib/main.dart
@@ -85,3 +85,44 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+drawer: Drawer(
+  child: ListView(
+    children: [
+      const DrawerHeader(
+        child: Text("Union Shop", style: TextStyle(fontSize: 24)),
+      ),
+      ListTile(
+        title: const Text("Home"),
+        onTap: () => Navigator.pop(context),
+      ),
+      ListTile(
+        title: const Text("Collections"),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (_) => const CollectionsPage()));
+        },
+      ),
+      ListTile(
+        title: const Text("Sale"),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const SalePage()));
+        },
+      ),
+      ListTile(
+        title: const Text("About Us"),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const AboutPage()));
+        },
+      ),
+      ListTile(
+        title: const Text("Login"),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const LoginPage()));
+        },
+      ),
+    ],
+  ),
+),
